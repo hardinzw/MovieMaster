1891b62bcfd9ab186846f4aff58d35749a72d4ab
Zach Hardin <hardinzw@gmail.com>
2023-03-09 13:30:29 -0500

    Add project files.

---
+++ MovieMaster/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MovieMaster.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
